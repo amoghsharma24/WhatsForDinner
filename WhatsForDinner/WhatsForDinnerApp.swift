c33f1959bd7a539f02a6095e6782a8c15239e636
Amogh Sharma <amoghsharma02@gmail.com>
2025-02-21 10:04:33 +1100

    Initial Commit

---
+++ WhatsForDinner/WhatsForDinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsForDinnerApp.swift
+//  WhatsForDinner
+//
+//  Created by Amogh Sharma  on 21/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct WhatsForDinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
